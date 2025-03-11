ee808e8b799903c88c4e23ba6a057b02c6b984d0
nahian28 <nahiansylhet@gmail.com>
2025-03-12 00:29:26 +0600

    f

--- page.php
+++ page.php
@@ -1,30 +1,11 @@
-<?php
-get_header();  // Includes the header.php file
-?>
-
-<section class="breadcumb-area py-5 text-center" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/img/footer.jpg');">
-    <div class="container">
-        <div class="grid">
-            <div class="col-md-12">
-                <h4><?php the_title();?></h4>
-                <ul>
-                    <li><a href="<?php echo site_url();?>">Home</a></li>
-                    <li><?php the_title();?></li>
-                </ul>
-            </div>
-        </div>
-    </div>
-</section>
+<?php get_header(); ?>
 
     <?php
     if (have_posts()) :
         while (have_posts()) : the_post();
-            // The content of the page
             the_content();
         endwhile;
     endif;
     ?>
 
-<?php
-get_footer();  // Includes the footer.php file
-?>
+<?php get_footer(); ?>
