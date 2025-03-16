d93f3f9f96b5f176de25bb889e971855527d9274
nahian28 <nahiansylhet@gmail.com>
2025-03-17 04:01:38 +0600

    Upda

--- header.php
+++ header.php
@@ -7,6 +7,31 @@
 <?php wp_head();?>
 <body <?php body_class();?>>
 
+<section class="mobile-header header-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="logo">
+                    <a href="<?php echo site_url();?>">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png">
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <?php
+                    wp_nav_menu( array(
+                        'theme_location' => 'primary', // The menu location registered earlier
+                        'container'       => 'nav',     // The HTML element to wrap the menu
+                        'container_class' => 'primary-menu', // CSS class for the container
+                        'menu_class'      => 'menu',   // CSS class for the <ul> element
+                    ) );
+                ?>
+
+            </div>
+        </div>
+    </div>
+</section>
+
 <section class="header-top py-3">
     <div class="container">
         <div class="row align-items-center">
